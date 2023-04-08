259e687252e472eb8e2a14063c365d33bc3aa3ac
Sebastian Diaz <sebba.2000@gmail.com>
2023-04-08 01:00:15 -0400

    deleted: hardcoded parameters

--- simulation.f90
+++ simulation.f90
@@ -9,52 +9,8 @@ program main
     real :: start_time, end_time, elapsed_time
 
     real(8), dimension(:,:), allocatable :: coordinates, Cr_coordinates, neighbors, spins
-    real(8), dimension(2, 3) :: primitiveCrI3, primitiveCrI2, vlatticeCrI3, vlatticeCrI2
-    real(8), dimension(8, 3) :: basis_CrI3 ! Basis vectors for the CrI3 structure.
-    real(8), dimension(6, 3) :: basis_CrI2 ! Basis vectors for the CrI2 structure.
-    character(len=2), dimension(8) :: elementsCrI3 = (/ 'Cr', 'Cr', 'I ', 'I ', 'I ', 'I ', 'I ', 'I ' /)
-    character(len=2), dimension(6) :: elementsCrI2 = (/ 'Cr', 'Cr', 'I ', 'I ', 'I ', 'I '/)
-    
-    ! Define the primiteve vectors for the CrI3 structure.
-    primitiveCrI3(1, :) = [6.8911914825, 0.0, 0.0]
-    primitiveCrI3(2, :) = [-3.4488059462999998, 5.9711638719, 0.0]
-    ! Define the basis vectors for the CrI3 structure.
-    basis_CrI3(1,:) = [3.42245459776438, 1.94691491308500, 9.72065830568899]   !Cr
-    basis_CrI3(2,:) = [-0.02339545180016, 3.93735766285690, 9.72080135688876]  !Cr
-    basis_CrI3(3,:) = [-1.03538548904423, 5.92724513968807, 11.28527832915810] !I 
-    basis_CrI3(4,:) = [2.20273590406674, 3.81437945298318, 11.29027271402490]  !I
-    basis_CrI3(5,:) = [5.64639472731378, 2.07416105561567, 11.29166984494761]  !I
-    basis_CrI3(6,:) = [0.97989374494890, 5.92913055695139, 8.15211676904270]   !I
-    basis_CrI3(7,:) = [1.19604098486642, 2.06898570270974, 8.15546418188776]   !I
-    basis_CrI3(8,:) = [4.64469384948989, 3.81029176486010, 8.15107346274473]   !I
-    
-    ! Define the primitive vectors for the CrI2 structure.
-    primitiveCrI2(1, :) = [3.9350889407837069, 0.0, 0.0]
-    primitiveCrI2(2, :) = [0.0, 7.793970515734439, -0.0155822353114624]
-    ! Degine the basis vectors for the CrI2 structure.
-    basis_CrI2(1,:) = [0.00000000000000, 1.18048232888026, 15.66708540526766]  !Cr
-    basis_CrI2(2,:) = [1.96754466301406, 5.07735235387682, 15.66009018229279]  !Cr
-    basis_CrI2(3,:) = [1.96754466301406, 2.14455628147048, 17.30559462841125]  !I 
-    basis_CrI2(4,:) = [1.96754466301406, 0.21647035484051, 14.02945080633759]  !I
-    basis_CrI2(5,:) = [0.00000000000000, 6.04092814648801, 17.29708073114964]  !I
-    basis_CrI2(6,:) = [0.00000000000000, 4.11332427420235, 14.02184874359694]  !I
-
-
-    !write(*,*) 'This program generates the structures of CrI3.'
-    !write(*,*) 'Please provide the number of unit cells for the x direction:'
-    !read(5,*) nx
-    !write(*,*) 'Please provide the number of unit cells for the y direction:'
-    !read(5,*) ny
-
-
-
-
-    ! Define the lattice vectors for periodic boundary conditions.
-    vlatticeCrI3(1,:) = [primitiveCrI3(1,1)*nx, primitiveCrI3(1,2)*nx, primitiveCrI3(1,3)*nx]
-    vlatticeCrI3(2,:) = [primitiveCrI3(2,1)*ny, primitiveCrI3(2,2)*ny, primitiveCrI3(2,3)*ny]
-    vlatticeCrI2(1,:) = [primitiveCrI2(1,1)*nx, primitiveCrI2(1,2)*nx, primitiveCrI2(1,3)*nx]
-    vlatticeCrI2(2,:) = [primitiveCrI2(2,1)*ny, primitiveCrI2(2,2)*ny, primitiveCrI2(2,3)*ny]
-
+  
+    call read_parameters()
     ! Calculate the number of atoms in the structure.
     natoms = nx*ny*8
     call cpu_time(start_time)
@@ -83,7 +39,7 @@ program main
     call cpu_time(end_time)
     elapsed_time = end_time - start_time
 
-    open(unit=14, file='data.txt', status="old", position='append', action="write")
+    open(unit=14, file=output_file, status="old", position='append', action="write")
     write(14, *) 'This is the time it took to run the program: ', elapsed_time
     close(14)
     write(6, *) 'This is the time it took to run the program: ', elapsed_time
@@ -231,8 +187,9 @@ function generate_spins(natoms) result(spins)
     integer :: choice, i
     real(8), dimension(3) :: spin
     
-    write(*,*) "Choose an option: (1) ramdom directions or (2) fixed direction"
-    read(*,*) choice
+    !write(*,*) "Choose an option: (1) ramdom directions or (2) fixed direction"
+    !read(*,*) choice
+    choice = 1
     
     if (choice == 1) then
         !Each spin is a different randomly generated normal vector.
@@ -366,9 +323,9 @@ subroutine simulation(mcs, CrAtoms, nx, ny, T, J, H, spins, neighbors)
     real(8), dimension(3) :: current_magnetization_vector, final_magnetization_vector
     
     
-    open(12, file='data.txt', status='unknown')
+    open(12, file=output_file, status='unknown')
     write(12,*) '#seed - mcs - temperature - CrAtoms - nx - ny - H'
-    write(12,*) '#',seed, mcs, T, CrAtoms, nx, ny, H
+    write(12,*) '#', seed, mcs, T, CrAtoms, nx, ny, H
     write(12,*) '#Mcs', 'Energy', 'Magnetization'
     !close(12)
     !stop

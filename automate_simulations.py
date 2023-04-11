a07e2a49d9070e52de08b5d49f22500d6b9f5f00
Sebastian Diaz <sebba.2000@gmail.com>
2023-04-11 17:35:00 -0400

    add confirmation for number of nohup calls

--- automate_simulations.py
+++ automate_simulations.py
@@ -28,28 +28,43 @@ class Data:
 
 
 
-seeds = [-111, -222, -333, -444, -555]
-
+#seeds = [-111, -222, -333, -444, -555]
+seeds = [-111]
 choice =input('Enter 1 to run xsimulation or 2 to run xgenerate: ')
 
 #Inputs
 initial_nxy = int(input('Enter the initial nx and ny (nx = ny): '))
 final_nxy = int(input('Enter the final nx and ny (nx = ny): '))
+step_n = int(input('Enter the step: '))
 mcs = int(input('Enter the number of Monte Carlo steps: '))
 temperature = int(input('Enter the temperature: '))
 print('Enter the spins orientation: ')
 spins_orientation = int(input('1 for all random and 2 for all the same:'))
 
 
-#Create nohup
-'''
-'''
+
+number_of_simulations = len(seeds)*len(list(range(initial_nxy, final_nxy + 1, step_n)))
 if choice == '1':
-    for nxy in range(initial_nxy, final_nxy + 1):
-        for seed in seeds:
-            Data(seed, mcs, temperature, nxy, nxy, spins_orientation).write_params()
-            subprocess.run(f'nohup ./xsimulation > ./stabilizer_output/n{nxy}t{temperature}s{seed}.output &', shell=True)
-            time.sleep(0.1) 
+    print('You are going to print the following values for N:')
+    print(list(range(initial_nxy, final_nxy + 1, step_n)))
+    print(' and you are going to print the following values for seeds:')
+    print(seeds)
+    print('That would call a total of ', number_of_simulations, ' simulations.')
+    print('Simulations : ', number_of_simulations)
+    print('Are you sure you want to continue?')
+    print('Enter 1 to continue or anything else to exit: ')
+    choice2 = int(input())
+    if choice2 == 1:
+        for nxy in range(initial_nxy, final_nxy + 1, step_n):
+            print(f'Running for nxy = {nxy}...')
+            for seed in seeds:
+                print(f'Running for seed = {seed}...')
+                Data(seed, mcs, temperature, nxy, nxy, spins_orientation).write_params()
+                subprocess.run(f'nohup ./xsimulation > ./stabilizer_output/n{nxy}t{temperature}s{seed}.output &', shell=True)
+                time.sleep(0.1) 
+    else: 
+        print('Exiting...')
+        exit()
 elif choice == '2':
     for nxy in range(initial_nxy, final_nxy + 1):
             Data(-111, mcs, temperature, nxy, nxy, spins_orientation).write_params()

c01c0a43e4007753c1bb9142beb1d678f6a932c2
Sebastian Diaz <sebba.2000@gmail.com>
2023-05-21 17:03:15 -0400

    Add support for CrI2.

--- Source/tLoop.f90
+++ Source/tLoop.f90
@@ -8,7 +8,9 @@ program tLoop
     real(8) :: beta, Cv, Chi
     real(8) :: avg_energy, avg_mag, avg_energy2, avg_mag2
     real(8) :: avg_mag_vec(3)
-    integer :: i
+    real(8), allocatable, dimension(:,:)  :: spins_ma, spins_mb
+    real(8) :: Ma, Mb, Ms, Ma_vec(3), Mb_vec(3), Ms_vec(3)
+    integer :: i, i2, index
 
 
 
@@ -18,7 +20,30 @@ program tLoop
 
 
     neighbors = read_neighbors(Cr_atoms)
-    spins = generate_spins(Cr_atoms, spins_orientation, initial_magnetization_vector)
+    if (compound == 'CrI3') then 
+        spins = generate_spins(Cr_atoms, spins_orientation, initial_magnetization_vector)
+    else 
+        spins_ma = generate_spins(Cr_atoms/2, spins_orientation, initial_magnetization_vector(:))
+        spins_mb = generate_spins(Cr_atoms/2, spins_orientation, -1 * initial_magnetization_vector(:))
+        allocate(spins(Cr_atoms, 3))
+        do i = 1, Cr_atoms/2
+            index = 2*i - 1
+            spins(index,:) = spins_ma(i,:)
+            Ma_vec = Ma_vec + spins_ma(i,:)
+            index = 2*i
+            spins(index,:) = spins_mb(i,:)
+            Mb_vec = Mb_vec + spins_mb(i,:)
+            
+        end do
+        Ma_vec = Ma_vec/Cr_atoms*2
+        Mb_vec = Mb_vec/Cr_atoms*2
+        Ms_vec = (Ma_vec/2 - Mb_vec/2)
+        write(6, *)'Ma:  ', sqrt(dot_product(Ma_vec, Ma_vec))!/Cr_atoms/2
+        write(6, *)'Mb:  ', sqrt(dot_product(Mb_vec, Mb_vec))!/Cr_atoms/2
+        write(6, *)'Ms:  ', sqrt(dot_product(Ms_vec, Ms_vec))!/Cr_atoms
+    end if
+
+
     T = iT
     H = iH
     call calculate_system_energy
@@ -50,6 +75,12 @@ program tLoop
     58 format(' dS= ', F3.1  )
     flush(13)
 
+    if (compound == 'CrI3') then
+        write(12, 77) "T", "M", "Mx", "My", "Mz", "E", "Cv", "Chi"
+    else 
+        write(12, 77) "T", "Ms", "Msx", "Msy", "Msz", "E", "Cv", "Chi"
+    end if
+    77 format(A12,4(1x,A9),1x,A14,1x,A16,1x,A16)
     do while (T < fT)
         do i = 1, mcs
             call metropolis_rng
@@ -63,21 +94,47 @@ program tLoop
             call metropolis_rng
             avg_energy = avg_energy + system_energy
             avg_energy2 = avg_energy2 + system_energy**2
-            avg_mag = avg_mag + sqrt(dot_product(mag_vec, mag_vec))
-            avg_mag2 = avg_mag2 + dot_product(mag_vec, mag_vec)
-            avg_mag_vec = avg_mag_vec + mag_vec
+            if (compound == 'CrI3') then
+                avg_mag = avg_mag + sqrt(dot_product(mag_vec, mag_vec))
+                avg_mag2 = avg_mag2 + dot_product(mag_vec, mag_vec)
+                avg_mag_vec = avg_mag_vec + mag_vec
+            else
+                do i2 = 1, Cr_atoms/2
+                    index = 2*i2 - 1
+                    spins_ma(i2,:) = spins(index,:) 
+                    Ma_vec = Ma_vec + spins_ma(i2,:)
+                    index = 2*i2
+                    spins_mb(i2,:) = spins(index,:) 
+                    Mb_vec = Mb_vec + spins_mb(i2,:)
+                end do
+                Ma_vec = Ma_vec/Cr_atoms*2
+                Mb_vec = Mb_vec/Cr_atoms*2
+                Ms_vec = (Ma_vec/2 - Mb_vec/2)
+                avg_mag = avg_mag + sqrt(dot_product(Ms_vec, Ms_vec))
+                avg_mag2 = avg_mag2 + dot_product(Ms_vec, Ms_vec)
+                avg_mag_vec = avg_mag_vec + Ms_vec
+            end if 
+        
+        
         end do
         avg_energy = avg_energy / neq
         avg_energy2 = avg_energy2 / neq
         avg_mag = avg_mag / neq
+        
+
         avg_mag2 = avg_mag2 / neq
         avg_mag_vec = avg_mag_vec / neq
         
         beta = 1 / T*kB
         Cv = beta**2 * (avg_energy2 - avg_energy**2)
         Chi = beta * (avg_mag2 - avg_mag**2)
-        write(12,34) T, avg_mag/Cr_atoms, avg_mag_vec(:)/Cr_atoms, avg_energy/Cr_atoms, Cv, Chi
-        34 format(F12.8,4(1x,F9.6),1x,F14.8,1x,E16.9,1x,E16.9)
+        if (compound == 'CrI3') then 
+            write(12,34) T, avg_mag/Cr_atoms, avg_mag_vec(:)/Cr_atoms, avg_energy/Cr_atoms, Cv, Chi
+            34 format(F12.8,4(1x,F9.6),1x,F14.8,1x,E16.9,1x,E16.9)
+        else 
+            write(12,35) T, avg_mag, avg_mag_vec(:), avg_energy/Cr_atoms, Cv, Chi
+            35 format(F12.8,4(1x,F9.6),1x,F14.8,1x,E16.9,1x,E16.9)
+        end if
         flush(12)
         T = T + dT
     end do
